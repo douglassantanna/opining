effc42e3a6de39cb762ceb9748581c702f80b52d
Douglas Santanna <douglbb1@gmail.com>
2022-05-15 23:23:36 -0300

    auth module, controller and service created

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,6 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { UsersModule } from './users/users.module';
       'mongodb+srv://root:root@cluster1.cjkxq.mongodb.net/opiningdb',
     ),
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
